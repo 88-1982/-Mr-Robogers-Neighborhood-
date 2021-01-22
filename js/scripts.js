4cc0c7a7a6cb676dd41232e16600f20c141f515b
De Aunte Hall <deauntehall@Des-MacBook-Pro.local>
2021-01-22 14:48:09 -0800

    add input tags and div tags to HTML. also a function to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
-function number();
-let numbers = [,1,10,11,12,13,14,15,16,17,18,19,];
-  alert("Beep!")
+function submitButton() {
+      //event.perventdefault();
+      let number1 = ("input[number=1]:checked").val();
+      let number2 = ("input[number=2]:checked").val();
+      let number3  = ("input[number=3 ]:checked").val();
+      let number4  = ("input[number=4 ]:checked").val();
+      let number5  = ("input[number=5 ]:checked").val();
+      let number6 = ("input[number=6 ]:checked").val();
+      let number7  = ("input[number=7 ]:checked").val();
+      let number8  = ("input[number=8 ]:checked").val();
+      let number9  = ("input[number=9 ]:checked").val();
+      let number10  = ("input[number=10 ]:checked").val();
+      let number11  = ("input[number=11 ]:checked").val();
+      let number12  = ("input[number=12 ]:checked").val();
+      let number13  = ("input[number=13 ]:checked").val();
+      let number14  = ("input[number=14 ]:checked").val();
+      let number15  = ("input[number=15 ]:checked").val();
+      let number16  = ("input[number=16 ]:checked").val();
+      let number17  = ("input[number=17 ]:checked").val();
+      let number18  = ("input[number=18 ]:checked").val();
+      let number19 = ("input[number=19 ]:checked").val();
+      let number20 = ("input[number=20 ]:checked").val();
+
 
 
 
@@ -8,3 +28,11 @@ let numbers = [,1,10,11,12,13,14,15,16,17,18,19,];
 
 
 }
+
+
+
+
+
+
+
+
